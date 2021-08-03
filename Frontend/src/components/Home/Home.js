e46a0fc434e19a66e897197f0f7a3259079a5ee2
aryan basu <aryanbasu005@gmail.com>
2021-08-03 19:54:29 +0530

    Update Home.js

--- Frontend/src/components/Home/Home.js
+++ Frontend/src/components/Home/Home.js
@@ -1,58 +1,56 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css'
 import TextField from '@material-ui/core/TextField'
 import InputAdornment from '@material-ui/core/InputAdornment'
 import { AiOutlineArrowRight } from 'react-icons/ai'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
-import {RiAddBoxLine} from 'react-icons/ri'
+import { RiAddBoxLine } from 'react-icons/ri'
 import { useHistory } from 'react-router-dom';
 import firebase from 'firebase';
+const Home = (props) => {
 
-const Home = () => {
+    let textInput = React.createRef();
     var user = firebase.auth().currentUser;
     var fullname, email;
     if (user !== null) {
-        fullname=user.displayName;
+        fullname =user.displayName.split(' ')[0];
         email = user.email;
+    }
+
+    const history = useHistory();
+    async function handleLogout() {
+        await firebase.auth().signOut();
+        history.push("/");
+    };
+
+    function handlejoin() {
+     const code=textInput.current.value;
+     if(code!=null)
+     {
+         history.push('/meeting/'+code)
+     }
       }
-
-const history = useHistory() ;
-         async function handleLogout () {
-            await firebase.auth().signOut();
-            history.push("/");
-            };
-
-            return (
+    return (
         <div>
             <div className="home">
                 <h4>Welcome {fullname} !</h4>
-                <TextField
-                    placeholder='Enter meeting code'
-                    id="outlined-start-adornment"
-                    InputProps={{
-                        endAdornment:
-                            <InputAdornment position="end">
-                                <IconButton className='input-btn'>
-                                    <AiOutlineArrowRight className='input-icons' />
-                                </IconButton>
-                            </InputAdornment>,
-                    }}
-                    variant="outlined"
-                />
+         
+                <input type="text" ref={textInput} placeholder="Enter Meeting code" /> 
+                <button onClick={handlejoin}>Join meet</button>
                 <div className='lines'>
                     <div className='line-1'></div>
                     <p>or</p>
                     <div className='line-1'></div>
                 </div>
 
-                <Button  onClick = { () => {  history.push( { pathname : '/preview'}) } } fullWidth={true} className='btn-meet'>
+                <Button onClick={() => { history.push({ pathname: '/preview' }) }} fullWidth={true} className='btn-meet'>
                     <InputAdornment>
-                            <RiAddBoxLine className='btn-icon'  />
+                        <RiAddBoxLine className='btn-icon' />
                     </InputAdornment>
                     New Meet
                 </Button>
-                <Button onClick={handleLogout} className='btn-meet'>Sign out</Button>
+                <Button onClick={handleLogout} className='btn mt-2'>Sign out</Button>
             </div>
         </div>
     );
